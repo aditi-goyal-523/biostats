2857c1ea196c516d0c31d0096fe9d26d5a874bd7
jonjudd <97633528+jonjudd@users.noreply.github.com>
2024-01-22 20:26:01 -0500

    Updated ORfunction & logit.plot

--- functions.R
+++ functions.R
@@ -81,3 +81,96 @@ breslowday.test = function(x, OR=NA, printORi.s=TRUE){
   return(unlist(list(OR = OR, Stat = X2, df = k-1, pvalue = pvalue)))
 }
 
+ORfunction = function(confidence,a,b,c,d) {
+  #enter confidence percent as a whole number, e.g. "95"
+  
+  OR = (a*d) / (b*c)
+  
+  lnOR = log(OR)
+  error = sqrt(1/a + 1/b + 1/c + 1/d)
+  Z = -qnorm((1 - confidence/100)/2)
+    #gives left hand Z score, multiply by negative
+  
+  lower = exp(lnOR - Z*error)
+  upper = exp(lnOR + Z*error)
+  
+  output = c(OR, lower, upper)
+  names(output) = c("OR", "lower", "upper")
+  return(output)
+}
+
+logit.plot = function(data,predictor,outcome,numIntervals=10,spline=TRUE,spar=0.7) {
+  #Check inputs
+  if (!is.character(predictor)) {stop("The predictor must be a string.")}
+  if (!is.character(outcome)) {stop("The outcome must be a string.")}
+  
+  #Initialize vectors
+  logits = rep(0,numIntervals)
+  means = rep(0,numIntervals)
+  
+  #Define outcome and predictor columns (renames them to make the code below simpler)
+  outcomeIndex = which(colnames(data)==outcome)
+  predictorIndex = which(colnames(data)==predictor)
+  names(data)[outcomeIndex] = "outcome"
+  names(data)[predictorIndex] = "predictor"
+  
+  #Check for missing data
+  if (any(is.na(data$predictor))) {stop("The predictor contains missing data.")}
+  if (any(is.na(data$outcome))) {stop("The outcome contains missing data.")}
+  
+  #Sort data in ascending order of predictor
+  data=data[order(data$predictor),]
+  
+  #Define the intervals
+  intervalLength = floor(dim(data)[1] / numIntervals)
+  #floor() enforces rounding down, in case numIntervals doesn't divide evenly into dim(data)[1]
+  
+  if ((dim(data)[1] / numIntervals) - floor(dim(data)[1] / numIntervals) != 0) {
+    warning("The number of intervals does not divide evenly into the size of the dataset. Some data points will be omitted.")
+  }
+  
+  #Define the starting index of each section
+  intervalStarts = c()
+  for (k in 1:numIntervals) {
+    intervalStarts[k] = 1 + (k-1)*intervalLength
+  }
+  
+  #Loop over each section
+  for (j in intervalStarts) {
+    positive=0
+    negative=0
+    sum=0
+    
+    #Loop over each data point in the section
+    for (i in c(j:(j+intervalLength-1))) {
+      if (data$outcome[i] == 1) {             #outcome variable is 1
+        positive = positive + 1
+      } else if (data$outcome[i] == 0) {      #outcome variable is 0
+        negative = negative + 1
+      } else {
+        stop("The outcome column must be binary.")
+      }
+      
+      sum = sum + data$predictor[i]   #adding the predictor values so we can calculate the mean
+    }
+    
+    x = positive/(positive+negative)        #calculates the probability
+    logits[1+round(j/intervalLength)] = log(x/(1-x))        #puts the logit in the index of j
+    means[1+round(j/intervalLength)] = sum/intervalLength   #puts the mean in the index of j 
+    
+  }
+  
+  #Check for infinite logits
+  if (any(is.infinite(logits))) {
+    warning("Infinite logits generated. Not all points will be plotted.")
+  }
+  
+  #Plot each logit at the corresponding predictor value
+  plot(means,logits,xlab=paste(predictor))
+  
+  #Plot a smoothing spline
+  if (spline==TRUE) {
+    spline = smooth.spline(means[!is.infinite(logits)], logits[!is.infinite(logits)], spar=spar)
+    lines(spline)  
+  }
+}
